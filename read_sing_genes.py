abc246a68fa02e1b35688117baff5972c7bcee0f
Ian Driver <driver.ian@gmail.com>
2015-01-23 14:05:38 -0800

    Organize files

--- read_sing_genes.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-def file_gene_list(path='/Users/idriver/RockLab-files/spc_ensembl/'):
-  file = 'new-spc-highPC1-2.txt'
-  gene_df = pd.read_csv(path+file, delimiter= '\t')
-  gene_list = gene_df['GeneID']
-  return gene_list
