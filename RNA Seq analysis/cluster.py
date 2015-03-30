0779308c28831598cad20cb1e1c469796b20e319
Ian Driver <driver.ian@gmail.com>
2015-03-30 15:34:17 -0700

    augment dendrogram and make json for d3 functions

--- RNA Seq analysis/cluster.py
+++ RNA Seq analysis/cluster.py
@@ -3,8 +3,13 @@ import numpy as np
 import pandas as pd
 import os
 import matplotlib.pyplot as plt
+import scipy
+import json
 from scipy.spatial.distance import pdist, squareform
-from scipy.cluster.hierarchy import linkage, dendrogram
+from scipy.cluster.hierarchy import fcluster, linkage, dendrogram, set_link_color_palette, to_tree, inconsistent
+import seaborn as sns
+from matplotlib.colors import rgb2hex, colorConverter
+from pprint import pprint
 
 path_to_file ='/Volumes/Seq_data/Pdgfra2_all_fpkm_analysis'
 
@@ -17,15 +22,126 @@ by_gene = pickle.load(fpbgene)
 fpgenelist = open(os.path.join(path_to_file,'fpkm_cuff_pdgfra2_outlier_gene_list.p'), 'rb')
 gene_list = pickle.load(fpgenelist)
 
-print by_cell.shape
+sns.set_palette('Set1', 10, 0.65)
+palette = sns.color_palette()
+set_link_color_palette(map(rgb2hex, palette))
+sns.set_style('white')
+
 df_by_cell = pd.DataFrame(by_cell, columns=gene_list, index=cell_list)
-print df_by_cell.shape
 row_dist = pd.DataFrame(squareform(pdist(df_by_cell, metric='euclidean')), columns=cell_list, index=cell_list)
-row_clusters = linkage(row_dist, method='complete')
+row_clusters = linkage(row_dist, metric='euclidean', method='ward')
 link_mat = pd.DataFrame(row_clusters,
              columns=['row label 1', 'row label 2', 'distance', 'no. of items in clust.'],
              index=['cluster %d' %(i+1) for i in range(row_clusters.shape[0])])
 row_dendr = dendrogram(row_clusters, labels=cell_list, leaf_rotation=90, leaf_font_size=8)
 
 
-plt.show()
+
+def augmented_dendrogram(*args, **kwargs):
+
+    ddata = dendrogram(*args, **kwargs)
+
+    if not kwargs.get('no_plot', False):
+        for i, d in zip(ddata['icoord'], ddata['dcoord']):
+            x = 0.5 * sum(i[1:3])
+            y = d[1]
+            if y >= 200000:
+                plt.plot(x, y, 'ro')
+                plt.annotate("%.3g" % y, (x, y), xytext=(0, -8),
+                         textcoords='offset points',
+                         va='top', ha='center')
+
+    plt.show()
+
+def cluster_indices(cluster_assignments):
+    n = cluster_assignments.max()
+    indices = []
+    for cluster_number in range(1, n + 1):
+        indices.append(np.where(cluster_assignments == cluster_number)[0])
+    return indices
+
+def clust_members(r_link, cutoff):
+    clust = fcluster(r_link,cutoff)
+    num_clusters = clust.max()
+    indices = cluster_indices(clust)
+    return  num_clusters, indices
+
+def print_clust_membs(indices, cell_list):
+    for k, ind in enumerate(indices):
+        print "cluster", k + 1, "is", [cell_list[x] for x in ind]
+
+def plot_tree(dendr, pos=None, save=False):
+    icoord = scipy.array(dendr['icoord'])
+    dcoord = scipy.array(dendr['dcoord'])
+    color_list = scipy.array(dendr['color_list'])
+    xmin, xmax = icoord.min(), icoord.max()
+    ymin, ymax = dcoord.min(), dcoord.max()
+    if pos:
+        icoord = icoord[pos]
+        dcoord = dcoord[pos]
+    for xs, ys, color in zip(icoord, dcoord, color_list):
+        plt.plot(xs, ys, color)
+    plt.xlim(xmin-10, xmax + 0.1*abs(xmax))
+    plt.ylim(ymin, ymax + 0.1*abs(ymax))
+    if save:
+        plt.savefig('plot_dendrogram.png')
+    plt.show()
+# Create a nested dictionary from the ClusterNode's returned by SciPy
+def add_node(node, parent):
+	# First create the new node and append it to its parent's children
+	newNode = dict( node_id=node.id, children=[] )
+	parent["children"].append( newNode )
+
+	# Recursively add the current node's children
+	if node.left: add_node( node.left, newNode )
+	if node.right: add_node( node.right, newNode )
+
+
+
+# Label each node with the names of each leaf in its subtree
+def label_tree( n ):
+	# If the node is a leaf, then we have its name
+	if len(n["children"]) == 0:
+		leafNames = [ id2name[n["node_id"]] ]
+
+	# If not, flatten all the leaves in the node's subtree
+	else:
+		leafNames = reduce(lambda ls, c: ls + label_tree(c), n["children"], [])
+
+	# Delete the node id since we don't need it anymore and
+	# it makes for cleaner JSON
+	del n["node_id"]
+
+	# Labeling convention: "-"-separated leaf names
+	n["name"] = name = "-".join(sorted(map(str, leafNames)))
+
+	return leafNames
+T= to_tree(row_clusters)
+
+# Create dictionary for labeling nodes by their IDs
+labels = list(df_by_cell.index)
+id2name = dict(zip(range(len(labels)), labels))
+
+# Initialize nested dictionary for d3, then recursively iterate through tree
+d3Dendro = dict(children=[], name="Root1")
+add_node( T, d3Dendro )
+label_tree( d3Dendro["children"][0] )
+print label_tree
+# Output to JSON
+json.dump(d3Dendro, open("d3-dendrogram.json", "w"), sort_keys=True, indent=4)
+with open("d3-dendrogram.json", "r") as f:
+    pprint(json.load(f))
+
+cutoff = 1.15
+cutoff_list =[]
+num_clusters = 0
+num_clusters_list = []
+while num_clusters != 1:
+    num_clusters, indices = clust_members(row_clusters, cutoff)
+    num_clusters_list.append(num_clusters)
+    cutoff_list.append(cutoff)
+    cutoff += 0.01
+print num_clusters_list
+
+#plot_tree(row_dendr)
+#augmented_dendrogram(row_clusters, labels=cell_list, leaf_rotation=90, leaf_font_size=8)
