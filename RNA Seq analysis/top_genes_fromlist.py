c39979b81ca5db631c4a8623ac73f015c045027c
Ian Driver <driver.ian@gmail.com>
2015-04-22 21:47:48 -0700

    can find top genes by how expression separates categories

--- RNA Seq analysis/top_genes_fromlist.py
+++ RNA Seq analysis/top_genes_fromlist.py
@@ -8,14 +8,15 @@ import matplotlib.pylab as pl
 from matplotlib.ticker import LinearLocator
 import seaborn as sns
 import numpy as np
+from operator import itemgetter
 
-shared_df = pd.DataFrame.from_csv('/Volumes/Seq_data/counts_sheppard_all/shared_genes_all.txt', sep='\t')
+shared_df = pd.DataFrame.from_csv('/Volumes/Seq_data/Spc2_counts/count_spc2_outlier_filtered.txt', sep='\t')
 shared_list = []
 for x in shared_df.index:
     shared_list.append(x.rstrip())
 
-path_to_file ='/Volumes/Seq_data/counts_sheppard_all'
-start_file_name = 'norm_cpm'
+path_to_file ='/Volumes/Seq_data/Spc2_counts'
+start_file_name = 'count_spc2'
 fpbcell = open(os.path.join(path_to_file, start_file_name+'_outlier_by_cell.p'), 'rb')
 by_cell = pickle.load(fpbcell)
 fpbcell.close()
@@ -29,7 +30,15 @@ fpgenelist = open(os.path.join(path_to_file, start_file_name+'_outlier_gene_list
 gene_list = pickle.load(fpgenelist)
 fpgenelist.close()
 
-def sort_df_bylist(num_to_show=50):
+def singular_gene_list(path=path_to_file, file = '/spc_2_top5000_pca.txt'):
+    gene_df = pd.read_csv(path+file, delimiter= '\t')
+    gene_list = gene_df['GeneID'].tolist()
+    final_list = []
+    for g in gene_list:
+        final_list.append(g.split('_')[0])
+    return final_list
+
+def sort_df_bylist(shared_list, num_to_show=50):
     find_exp_list =[]
     for g in shared_list:
         if g in list(by_gene.columns.values):
@@ -41,9 +50,44 @@ def sort_df_bylist(num_to_show=50):
     plot_df = shared_g_df[sorted_df.index.values[0:num_to_show]]
     print sorted_df.index.values[0:num_to_show]
 
-gene_list_toplot = ['Pdgfra', 'Pdgfrb', 'Col1a1', 'Acta2', 'Col3a1', 'Ctss', 'Cd48', 'Ptprc', 'Tgfbr1', 'Tgfbr2', 'Tgfbr3']
+#a list of genes to search for ranking categories (how well does expression level define categories)
+#split_on='sybol to split cell name', pos=index number of category name after split, cat_name=[list of category names]
+def find_gen_rank(gen_list, split_on='_', pos=1, cat_name=['d4pnx', 'ctrl']):
 
-term_to_sort = 'Col1a1'
+    score_tup =[]
+    for g in gen_list:
+        sorted_df = by_cell.sort([g])
+        g_df = sorted_df[g]
+        ranked_cells = sorted_df.index.values
+        ranked_cat = [x.split(split_on)[pos] for x in ranked_cells]
+        div_by = int(len(ranked_cat)/len(cat_name))
+        start = 0
+        last_div = div_by
+        score_list = np.zeros(len(cat_name)*len(cat_name))
+        store_ind = 0
+        for i,n in enumerate(cat_name):
+            if last_div > len(ranked_cat):
+                last_div = len(ranked_cat)
+            for z in range(len(cat_name)):
+                score1 = len([x for x in ranked_cat[start:last_div] if x == cat_name[z]])
+                tot = len([x for x in ranked_cat if x == cat_name[z]])
+                res_score = float(score1)/float(tot)
+                score_list[store_ind] = res_score
+                store_ind+=1
+
+            start= last_div
+            last_div +=div_by
+        if max(score_list) >= 0.7 and np.count_nonzero(g_df.values) >= 15:
+            score_tup.append((g,max(score_list)))
+    score_tup.sort(key=itemgetter(1), reverse=True)
+    score_df = pd.DataFrame(score_tup, columns=['GeneID', 'Maxscore'])
+    score_df.to_csv(os.path.join(path_to_file, 'sep_score_genes_fromPCA.txt'), sep = '\t', index=False)
+
+
+
+gene_list_toplot = ['Cdk1', 'G0s2', 'Dixdc1']
+
+term_to_sort = 'Dixdc1'
 sorted_df = by_cell.sort([term_to_sort])
 
 selected_df = sorted_df[gene_list_toplot]
@@ -58,7 +102,7 @@ def plot_genes(df_to_plot, log=False):
     xlabels = df_to_plot.index.values
     ax.xaxis.set_minor_locator(LinearLocator(numticks=len(xlabels)))
     ax.set_xticklabels(xlabels, minor=True, rotation='vertical')
-    ax.tick_params(axis='x', labelsize=5)
+    ax.tick_params(axis='x', labelsize=1)
     ax.legend(loc=1,prop={'size':6})
     plt.show()
 
@@ -66,4 +110,5 @@ def plot_genes(df_to_plot, log=False):
     cg = sns.clustermap(df_to_plot.transpose(), z_score=0)
     plt.show()
 
-plot_genes(selected_df, log=True)
+plot_genes(selected_df, log=False)
+#find_gen_rank(singular_gene_list())
