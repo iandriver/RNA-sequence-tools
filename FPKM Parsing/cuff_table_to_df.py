493e873f19eb76385fca913b704979dfa97dadf5
Ian Driver <driver.ian@gmail.com>
2015-04-29 18:45:36 -0700

    easier path input

--- FPKM Parsing/cuff_table_to_df.py
+++ FPKM Parsing/cuff_table_to_df.py
@@ -2,7 +2,11 @@ import os
 import cPickle as pickle
 import pandas as pd
 
-cuff_df = pd.DataFrame.from_csv('/Volumes/Seq_data/results_macs_pnx/sc_expr_out_macs/genes.fpkm_table', sep='\t')
 
-with open('/Volumes/Seq_data/results_macs_pnx/cuff_fpkm_table.p', 'wb') as fp:
+
+path_to_file= '/Volumes/Seq_data/counts_sheppard_all/cpm_norm_liver_kidney_1'
+file_name = 'sheppard_normalized_cpm_liver_kidney.txt'
+cuff_df = pd.DataFrame.from_csv(os.path.join(path_to_file,file_name), sep='\t')
+
+with open(os.path.join(path_to_file,file_name.strip('.txt')+'.p'), 'wb') as fp:
   pickle.dump(cuff_df, fp)
