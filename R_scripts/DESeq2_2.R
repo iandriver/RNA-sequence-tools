29a6172fcaec422a6c4e8bd9c0b530c52adbbfc6
Ian Driver <driver.ian@gmail.com>
2015-09-23 00:36:15 -0700

    fix cvs reader

--- R_scripts/DESeq2_2.R
+++ R_scripts/DESeq2_2.R
@@ -1,10 +1,10 @@
 run_deseq2 <- function(file_path, save_name){
 library(DESeq2)
-raw.data <- read.table(file = file_path, header=TRUE)
+raw.data <- read.csv(file = file_path, header=TRUE, sep="\t", row.names=1)
 counts <- raw.data[ , -c(1,ncol(raw.data)) ]
 rownames(counts) <- row.names(raw.data)
 group <- c()
-for (x in colnames( counts )){if(grepl('ctrl',x)){ group <- append(group, 'ctrl')} else if(grepl('pnx',x)){ group <- append(group,'pnx')} }
+for (x in colnames( counts )){if(grepl('BU3',x)){ group <- append(group, 'BU3')} else if(grepl('ips17',x)){ group <- append(group,'ips17')} }
 print(colnames(counts))
 print(group)
 samples <- data.frame(row.names=colnames(counts), condition=as.factor(c(group)))
--- R_scripts/make_cpm.R
+++ R_scripts/make_cpm.R
@@ -1,6 +1,6 @@
 library(edgeR)
 make_cpm <- function(path_to_file,file_base_name){
-	raw.data <- read.table(file = path_to_file, header=TRUE)
+	raw.data <- read.csv(file = file_path, header=TRUE, sep="\t", row.names=1)
 	counts <- raw.data[ , -c(1,ncol(raw.data)) ]
 	rownames( counts ) <- row.names(raw.data)
 	cds <- DGEList( counts , group = colnames(counts) )
