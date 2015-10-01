b6825c3890999b6f737c14c796cc73d0c50ba246
Ian Driver <driver.ian@gmail.com>
2015-09-30 22:38:21 -0700

    more dispersion estimates

--- R_scripts/DESeq2_2.R
+++ R_scripts/DESeq2_2.R
@@ -4,12 +4,17 @@ raw.data <- read.csv(file = file_path, header=TRUE, sep="\t", row.names=1)
 counts <- raw.data[ , -c(1,ncol(raw.data)) ]
 rownames(counts) <- row.names(raw.data)
 group <- c()
-for (x in colnames( counts )){if(grepl('BU3',x)){ group <- append(group, 'BU3')} else if(grepl('ips17',x)){ group <- append(group,'ips17')} }
+for (x in colnames( counts )){if(grepl('B_',x)){ group <- append(group, 'Basal')} else if(grepl('L_',x)){ group <- append(group,'Luminal')} }
 print(colnames(counts))
 print(group)
 samples <- data.frame(row.names=colnames(counts), condition=as.factor(c(group)))
 dds <- DESeqDataSetFromMatrix(countData = counts, colData=samples, design=~condition)
 dds <- DESeq(dds)
+dds <- estimateSizeFactors(dds)
+dds <- estimateDispersions(dds)
+dds <- nbinomWaldTest(dds)
+matrix <- counts(dds,normalized=TRUE)
+write.table(matrix, file=paste("DESeq_", save_name,"matrix_norm.txt", sep = "_"), sep="\t")
 saveRDS(dds, paste(save_name,".rds"))
 plotDispEsts(dds)
 return(dds)}
