4c66267c41bc7430d9d95a51c370b81f3fc09926
Ian Driver <driver.ian@gmail.com>
2015-10-06 20:29:03 -0700

    more filtering options for clustering

--- RNA_Seq_analysis/cluster.py
+++ RNA_Seq_analysis/cluster.py
@@ -5,6 +5,7 @@ import os
 import matplotlib
 matplotlib.use('QT4Agg')
 import matplotlib.pyplot as plt
+from matplotlib.ticker import LinearLocator
 import scipy
 import json
 from sklearn.decomposition import PCA as skPCA
@@ -16,34 +17,98 @@ from pprint import pprint
 
 
 #base path to pickle files with fpkm or count matrix
-path_to_file = '/Volumes/Seq_data/results_pdgfra_all_n2/cuffnorm_pdgfra_1_and_2'
+path_to_file = '/Volumes/Seq_data/count-picard_combined_ips17_BU3'
 #for labeling all output files
-start_file_name = 'pdgra_all_low_hi'
+start_file_name = 'ips17_BU3_normERCC'
 
-fpbcell = open(os.path.join(path_to_file, start_file_name+'_outlier_by_cell.p'), 'rb')
-by_cell = pickle.load(fpbcell)
-fpbcell.close()
-fpcelllist = open(os.path.join(path_to_file, start_file_name+'_outlier_cell_list.p'), 'rb')
-cell_list = pickle.load(fpcelllist)
-fpcelllist.close()
-fpbgene = open(os.path.join(path_to_file, start_file_name+'_outlier_by_gene.p'), 'rb')
-by_gene = pickle.load(fpbgene)
-fpbgene.close()
-fpgenelist = open(os.path.join(path_to_file, start_file_name+'_outlier_gene_list.p'), 'rb')
-gene_list = pickle.load(fpgenelist)
-fpgenelist.close()
+make_go_matrix = False
+
+#load file gene
+by_cell = pd.DataFrame.from_csv(os.path.join(path_to_file,'ips_hu_normalized_Genes_by_ERCC.txt'), sep='\t')
+by_gene = by_cell.transpose()
+#create list of genes
+gene_list = by_cell.index.tolist()
+#create cell list
+cell_list = [x for x in list(by_cell.columns.values)]
 
 
-cell_dist = pdist(np.array(by_cell), metric='euclidean')
-row_dist = pd.DataFrame(squareform(cell_dist), columns=cell_list, index=cell_list)
-row_clusters = linkage(row_dist, metric='euclidean', method='ward')
-link_mat = pd.DataFrame(row_clusters,
-             columns=['row label 1', 'row label 2', 'distance', 'no. of items in clust.'],
-             index=['cluster %d' %(i+1) for i in range(row_clusters.shape[0])])
-row_dendr = dendrogram(row_clusters, labels=cell_list, leaf_rotation=90, leaf_font_size=8)
+df_by_gene1 = pd.DataFrame(by_gene, columns=gene_list, index=cell_list)
+df_by_cell1 = pd.DataFrame(by_cell, columns=cell_list, index=gene_list)
 
-plt.savefig(os.path.join(path_to_file,'dendrogram_gene.png'))
-plt.clf()
+
+def make_new_matrix(org_matrix_by_cell, gene_list_file):
+    split_on='_'
+    gene_df = pd.read_csv(os.path.join(path_to_file, gene_list_file), delimiter= '\t')
+    gene_list = gene_df['GeneID'].tolist()
+    group_list = gene_df['GroupID'].tolist()
+    gmatrix_df = org_matrix_by_cell[gene_list]
+    cmatrix_df = gmatrix_df.transpose()
+    cell_list1 = []
+    for cell in cmatrix_df.columns.values:
+        if exclude:
+            if cell.split(split_on)[1] == 'ctrl' or cell.split(split_on)[1] == 'pnx':
+                if cell.split(split_on)[2][0] =='C':
+                    print cell, 'cell'
+                    cell_list1.append(cell)
+        else:
+            cell_list1.append(cell)
+    new_cmatrix_df = cmatrix_df[cell_list1]
+    new_gmatrix_df = new_cmatrix_df.transpose()
+    return new_cmatrix_df, new_gmatrix_df
+if make_go_matrix:
+    df_by_cell2, df_by_gene2 = make_new_matrix(df_by_gene1, gene_file_source)
+else:
+    df_by_cell2, df_by_gene2 = df_by_cell1, df_by_gene1
+
+
+def preprocess_df(np_by_cell, gen_list, number_expressed=4):
+    g_todelete = []
+    print gen_list
+    for g1, gene in enumerate(np_by_cell):
+        cells_exp = (gene >= 1.0).sum()
+        print cells_exp
+        if cells_exp < number_expressed:
+            g_todelete.append(g1)
+    g1_todelete = sorted(g_todelete, reverse = True)
+    print np_by_cell.shape
+    for pos in g1_todelete:
+        if type(gen_list[pos]) != float:
+            print 'Gene '+gen_list[pos]+' not expressed in '+str(number_expressed)+' cells.'
+            pass
+        del gen_list[pos]
+    n_by_cell = np.delete(np_by_cell, g1_todelete, axis=0)
+    print n_by_cell.shape
+    return n_by_cell, gen_list
+
+np_by_cell2 = np.array(df_by_cell2.values, dtype='f')
+gen_list = df_by_cell2.index.tolist()
+np_by_cell, n_gene_list = preprocess_df(np_by_cell2, gen_list)
+df_by_gene = pd.DataFrame(np_by_cell.transpose(), index = df_by_cell2.columns.values, columns= n_gene_list)
+df_by_cell = df_by_gene.transpose()
+log2_df = np.log2(df_by_cell+1)
+
+log2_df2= pd.DataFrame(log2_df.convert_objects(convert_numeric=True))
+print log2_df2.columns.values
+log_mean = log2_df.mean(axis=0).order(ascending=False)
+log2_sorted = log2_df.reindex_axis(log2_df.mean(axis=0).order(ascending=False).index, axis=1)
+ax = sns.boxplot(data=log2_sorted, whis= .75, notch=True)
+ax = sns.stripplot(x=log2_sorted.columns.values, y=log2_sorted.mean(axis=0), size=4, jitter=True, edgecolor="gray")
+xtickNames = plt.setp(ax, xticklabels=log2_sorted.columns.values)
+plt.setp(xtickNames, rotation=90, fontsize=9)
+plt.show()
+
+def run_cluster(by_gene_matrix):
+    cell_dist = pdist(np.array(by_gene_matrix), metric='euclidean')
+    row_dist = pd.DataFrame(squareform(cell_dist), columns=cell_list, index=cell_list)
+    row_clusters = linkage(row_dist, metric='euclidean', method='ward')
+    link_mat = pd.DataFrame(row_clusters,
+                 columns=['row label 1', 'row label 2', 'distance', 'no. of items in clust.'],
+                 index=['cluster %d' %(i+1) for i in range(row_clusters.shape[0])])
+    row_dendr = dendrogram(row_clusters, labels=cell_list, leaf_rotation=90, leaf_font_size=8)
+
+    plt.savefig(os.path.join(path_to_file,'dendrogram_gene.png'))
+    plt.clf()
+    return cell_dist, row_dist, row_clusters, link_mat, row_dendr
 
 def augmented_dendrogram(*args, **kwargs):
     plt.clf()
@@ -131,11 +196,11 @@ def label_tree(n, id2name):
     return leafNames
 
 #Makes labeled json tree for visulaization in d3
-def make_tree_json(row_clusters, df_by_cell):
+def make_tree_json(row_clusters, df_by_gene):
     T= to_tree(row_clusters)
 
     # Create dictionary for labeling nodes by their IDs
-    labels = list(df_by_cell.index)
+    labels = list(df_by_gene.index)
     id2name = dict(zip(range(len(labels)), labels))
 
     # Initialize nested dictionary for d3, then recursively iterate through tree
@@ -145,7 +210,8 @@ def make_tree_json(row_clusters, df_by_cell):
     # Output to JSON
     json.dump(d3Dendro, open(os.path.join(path_to_file,"d3-dendrogram.json"), "w"), sort_keys=True, indent=4)
 
-make_tree_json(row_clusters, by_cell)
+    return cc
+
 
 #makes
 def find_twobytwo(cc, threshold_num = 14):
@@ -165,8 +231,8 @@ def find_twobytwo(cc, threshold_num = 14):
     for v, k in pair_dict.items():
         cell_list1 = [x.strip('\n') for x in k[0]]
         cell_list2 = [xx.strip('\n') for xx in k[1]]
-        df_by_cell_1 = by_gene[cell_list1]
-        df_by_cell_2 = by_gene[cell_list2]
+        df_by_cell_1 = df_by_cell[cell_list1]
+        df_by_cell_2 = df_by_cell[cell_list2]
         df_by_gene_1 = df_by_cell_1.transpose()
         df_by_gene_2 = df_by_cell_2.transpose()
         for g in gene_list:
@@ -180,8 +246,8 @@ def find_twobytwo(cc, threshold_num = 14):
     sig_just_genes = [sig[0] for sig in sig_gene_list]
     return sig_just_genes
 
-def plot_PCA(sig_just_genes):
-    sig_by_cell = by_cell[sig_just_genes]
+def plot_PCA(gene_list_filter, df_by_gene):
+    sig_by_cell = df_by_gene[gene_list_filter]
     clf = skPCA(2)
     np_by_gene = np.asarray(sig_by_cell)
     by_gene_trans = clf.fit_transform(np_by_gene)
@@ -197,7 +263,7 @@ def plot_PCA(sig_just_genes):
         elif pc_2_gene_list[i] not in top_pca_list and pc_2_gene_list[i][0:2] != 'Rp':
             top_pca_list.append(pc_2_gene_list[i])
     print top_pca_list[0:150]
-    top_by_cell = by_cell[top_pca_list[0:150]]
+    top_by_cell = df_by_gene[top_pca_list[0:150]]
     clf_top = skPCA(n_components=2)
     np_top_gene = np.asarray(top_by_cell.transpose())
     print top_by_cell
@@ -219,13 +285,15 @@ def plot_PCA(sig_just_genes):
     sns.set_palette("RdBu_r", 10, 1)
     return top_pca_list
 
-def clust_heatmap(top_pca_list, num_to_plot=75):
-    cg = sns.clustermap(by_cell[top_pca_list[0:num_to_plot]].transpose(), z_score=0)
+def clust_heatmap(top_pca_list, df_by_gene, num_to_plot=75):
+    cg = sns.clustermap(df_by_gene[top_pca_list[0:num_to_plot]].transpose(), z_score=0)
     plt.show()
     plt.savefig(os.path.join(path_to_file,'clustermap_1.png'), bbox_inches='tight')
 
 #plot_tree(row_dendr)
+'''cell_dist, row_dist, row_clusters, link_mat, row_dendr = run_cluster(df_by_gene)
+cc = make_tree_json(row_clusters, df_by_gene)
 sig_gene_list = find_twobytwo(cc)
-top_pca = plot_PCA(sig_gene_list)
-clust_heatmap(top_pca)
-augmented_dendrogram(row_clusters, labels=cell_list, leaf_rotation=90, leaf_font_size=8)
+top_pca = plot_PCA(sig_gene_list, df_by_gene)
+clust_heatmap(top_pca, df_by_gene)
+augmented_dendrogram(row_clusters, labels=cell_list, leaf_rotation=90, leaf_font_size=8)'''
