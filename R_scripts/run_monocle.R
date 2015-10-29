1ae831cba475f91da8aa7920db40422dd0ce94c7
Ian Driver <driver.ian@gmail.com>
2015-10-29 11:15:06 -0700

    save plots

--- R_scripts/run_monocle.R
+++ R_scripts/run_monocle.R
@@ -4,63 +4,45 @@ run_monocle <- function(dir){
 	library(plyr)
 	setwd(dir)
 	fpkm_matrix <- read.delim("goterms_monocle_count_matrix.txt", row.names=1)
-	sample_sheet <- read.delim("cell_feature_data.txt", row.names=1)
-	feature_sheet <- read.delim("gene_feature_data.txt", row.names=1)
-<<<<<<< Updated upstream
-=======
-	head(fpkm_matrix)
-	head(sample_sheet)
-	head(feature_sheet)
->>>>>>> Stashed changes
+	sample_sheet <- read.delim("cell_feature_data.txt")
+	feature_sheet <- read.delim("gene_feature_data.txt")
+	row.names(sample_sheet) <- sample_sheet$tracking_id
+	row.names(feature_sheet) <- feature_sheet$GeneID
 	colnames(fpkm_matrix) <- row.names(sample_sheet)
 	row.names(fpkm_matrix) <- row.names(feature_sheet)
 	pd <- new("AnnotatedDataFrame", data = sample_sheet)
 	fd <- new("AnnotatedDataFrame", data = feature_sheet)
 	my_data <- newCellDataSet(as.matrix(fpkm_matrix), phenoData = pd, featureData=fd)
 	my_data <- detectGenes(my_data, min_expr = 1)
-	expressed_genes <- row.names(subset(fData(my_data), num_cells_expressed >= 50))
-<<<<<<< Updated upstream
-	valid_cells <- row.names(subset(pData(my_data), total_mass>=100000 & single_cell == 'yes' ))
-	my_data <- my_data[,valid_cells]
-	L <- log(exprs(my_data[expressed_genes,]))
-	melted_dens_df <- melt(t(scale(t(L))))
-	pdf("monocle.pdf")
-	qplot(value, geom="density", data=melted_dens_df) + stat_function(fun = dnorm, size=0.5, color='red') + xlab("Standardized log(FPKM)") + ylab("Density")
-	marker_genes <- row.names(subset(fData(my_data), GeneID %in% c('Scnn1a', 'Pdpn', 'Hopx', 'Aqp5', 'Ager')))
-=======
+	expressed_genes <- row.names(subset(fData(my_data), num_cells_expressed >= 10))
+
 	valid_cells <- row.names(subset(pData(my_data), total_mass>=100000 & single_cell=='yes' & per_mapped >=50))
 	my_data <- my_data[,valid_cells]
 	L <- log(exprs(my_data[expressed_genes,]))
 	melted_dens_df <- melt(t(scale(t(L))))
 	pdf("qplot_density.pdf")
 	qplot(value, geom="density", data=melted_dens_df) + stat_function(fun = dnorm, size=0.5, color='red') + xlab("Standardized log(FPKM)") + ylab("Density")
-	marker_genes <- row.names(subset(fData(my_data), tracking_id %in% c('Scnn1a', 'Pdpn', 'Hopx', 'Aqp5', 'Ager')))
->>>>>>> Stashed changes
+	dev.off()
+	marker_genes <- row.names(subset(fData(my_data), GeneID %in% c('Scnn1a', 'Pdpn', 'Hopx', 'Aqp5', 'Ager', 'Stat3', 'Jag1', 'Hbegf', 'Areg', 'Gpcr5a', 'Sftpc', 'Sftpd', 'Lamp3', 'Hes1', 'Krt8', 'Krt18', 'Tnfrsf12a', 'Myc', 'Abca1')))
 	diff_test_res_cond <- differentialGeneTest(my_data[marker_genes,], fullModelFormulaStr="expression~condition")
 	diff_test_res_day <- differentialGeneTest(my_data[marker_genes,], fullModelFormulaStr="expression~day")
 	sig_genes_cond <- subset(diff_test_res_cond, qval < 0.1)
 	sig_genes_day <- subset(diff_test_res_day, qval < 0.1)
 	sig_genes_cond <- merge(fData(my_data), sig_genes_cond, by="row.names")
 	sig_genes_day <- merge(fData(my_data), sig_genes_day, by="row.names")
-<<<<<<< Updated upstream
 	sig_genes_cond[,c("GeneID", "pval", "qval")]
 	sig_genes_day[,c("GeneID", "pval", "qval")]
-	type1 <- my_data[row.names(subset(fData(my_data),GeneID  %in% c("Ager", "Aqp5","Pdpn"))),]
-	plot_genes_jitter(type1, grouping="condition", ncol=3)
-	diff_test_res <- differentialGeneTest(my_data[expressed_genes,], fullModelFormulaStr="expression~condition")
+	diff_test_res <- differentialGeneTest(my_data[expressed_genes,], fullModelFormulaStr="expression~day")
 	ordering_genes <- row.names (subset(diff_test_res, qval < 0.01))
 	ordering_genes <- intersect(ordering_genes, expressed_genes)
 	my_data <- setOrderingFilter(my_data, ordering_genes)
 	my_data <- reduceDimension(my_data, use_irlba=FALSE)
-	my_data <- orderCells(my_data, num_paths=2, reverse=TRUE)
-	plot_spanning_tree(my_data)
+	my_data <- orderCells(my_data, num_paths=3, reverse=FALSE)
+	pdf('spanning_tree_plot.pdf')
+	plot_spanning_tree(my_data, x = 1, y = 2, color_by = "State", show_tree = TRUE, show_backbone = TRUE, backbone_color = "black", markers = NULL, show_cell_names = TRUE, cell_name_size = 2)
 	dev.off()
-	saveRDS(my_data, "monocle_data_spc2_n2.rds")
-=======
-	sig_genes_cond[,c("gene_short_name", "pval", "qval")]
-	sig_genes_day[,c("gene_short_name", "pval", "qval")]
-	type1 <- my_data[row.names(subset(fData(my_data),tracking_id %in% c("Ager", "Aqp5","Pdpn"))),]
-	plot_genes_jitter(type1, grouping="condition", ncol=3)
+	pdf('marker_genes_psuedotime.pdf')
+	plot_genes_in_pseudotime(my_data[marker_genes,], min_expr = NULL, cell_size = 0.75, nrow = NULL, ncol = 2, panel_order = NULL, color_by = "State", trend_formula = "adjusted_expression ~ sm.ns(Pseudotime, df=3)", label_by_short_name = TRUE)
 	dev.off()
->>>>>>> Stashed changes
+	saveRDS(my_data, "monocle_data_spc_sca1.rds")
 	}
