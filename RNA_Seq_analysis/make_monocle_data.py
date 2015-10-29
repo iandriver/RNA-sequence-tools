378e9ef5c5d4112429cc219c9f9ee7d16ebc5c30
Ian Driver <driver.ian@gmail.com>
2015-10-29 11:14:38 -0700

    simple monocle creation with plate name mapping

--- RNA_Seq_analysis/make_monocle_data.py
+++ RNA_Seq_analysis/make_monocle_data.py
@@ -11,11 +11,12 @@ import numpy as np
 from operator import itemgetter
 
 #the file path where gene list will be and where new list will output
-path_to_file = '/Volumes/Seq_data/cuffnorm_sca_spc_combined'
+path_to_file = '/Volumes/Seq_data/cuffnorm_hu_ht280_combined_rename'
 #name of file containing gene
 gene_file_source = 'go_search_genes_lung_all.txt'
+plate_map = 'plate_map_grid.txt'
 
-base_name = 'combined_sca_spc'
+base_name = 'hu_HT280_combined_renamed'
 #load file gene
 by_cell = pd.DataFrame.from_csv(os.path.join(path_to_file, base_name+'_outlier_filtered.txt'), sep='\t')
 by_gene = by_cell.transpose()
@@ -24,91 +25,89 @@ gene_list = by_cell.index.tolist()
 #create cell list
 cell_list = [x for x in list(by_cell.columns.values)]
 
+plate_map_df = pd.DataFrame.from_csv(os.path.join(path_to_file, plate_map), sep='\t')
+def ret_loading_pos(pos, plate_map_df):
+    let_dict = {'A':0, 'B':1, 'C':2, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7}
+    let = pos[0]
+    num = pos[1:]
+    num_col = plate_map_df[num]
+    cnum = num_col.iloc[let_dict[let]]
+    return int(cnum.strip('C'))
 
 df_by_gene1 = pd.DataFrame(by_gene, columns=gene_list, index=cell_list)
 df_by_cell1 = pd.DataFrame(by_cell, columns=cell_list, index=gene_list)
 
-def make_new_matrix(org_matrix_by_cell, gene_list_file, rankscore=['pnx', 'ctrl']):
+def make_new_matrix(org_matrix_by_cell, gene_list_file):
     split_on='_'
     gene_df = pd.read_csv(os.path.join(path_to_file, gene_list_file), delimiter= '\t')
     gene_list = gene_df['GeneID'].tolist()
     group_list = gene_df['GroupID'].tolist()
     gmatrix_df = org_matrix_by_cell[gene_list]
     cmatrix_df = gmatrix_df.transpose()
-    cell_list1 = []
-    for cell in cmatrix_df.columns.values:
-        if cell.split(split_on)[1] == 'ctrl' or cell.split(split_on)[1] == 'pnx':
-            if cell.split(split_on)[2][0] =='C':
-                print cell, 'cell'
-                cell_list1.append(cell)
-    new_cmatrix_df = cmatrix_df[cell_list1]
-    new_gmatrix_df = new_cmatrix_df.transpose()
-    new_cmatrix_df.to_csv(os.path.join(path_to_file, 'goterms_monocle_count_matrix.txt'), sep = '\t', index_col=0)
-    pos=1
-    cat_name=['pnx', 'ctrl']
-    score_tup =[]
-    for gene, go_term in zip(gene_list, group_list):
-        sorted_df = new_gmatrix_df.sort([gene])
-        score_on = 'pnx'
-        g_df = sorted_df[gene]
-        ranked_cells = sorted_df.index.values
-        ranked_cat = [x.split(split_on)[pos] for x in ranked_cells]
-        div_by = int(len(ranked_cat)/len(cat_name))
-        start = div_by *(len(cat_name)-1)
-        score1 = len([x for x in ranked_cat[start:len(ranked_cat)] if x == score_on])
-        tot = len([x for x in ranked_cat if x == score_on])
-        res_score = float(score1)/float(tot)
-        score = "%.3f" % res_score
-        score_tup.append((gene, gene, float(score), go_term))
-    score_df = pd.DataFrame(score_tup, columns=['', 'GeneID', 'Rankscore', 'GroupID'])
-    score_df.to_csv(os.path.join(path_to_file, 'gene_feature_data.txt'), sep = '\t', index=False)
+    score_df = pd.DataFrame(zip(gene_list, group_list), columns=['GeneID', 'GroupID'])
     sample_data = pd.read_csv(os.path.join(path_to_file, 'samples.table'), delimiter= '\t', index_col=0)
     by_sample = sample_data.transpose()
-    map_data = pd.read_csv(os.path.join(path_to_file, 'results_spc_d0_4_7_align.txt'), delimiter= '\t', index_col=0)
+    map_data = pd.read_csv(os.path.join(path_to_file, 'results_'+base_name+'_align.txt'), delimiter= '\t', index_col=0)
     by_cell_map = map_data.transpose()
-    loading_data = pd.read_csv(os.path.join(path_to_file, 'Cell_loading_SPC_all.txt'), delimiter= '\t', index_col=0)
+    loading_data = pd.read_csv(os.path.join(path_to_file, 'Cell_loading_hu_ht280_all.txt'), delimiter= '\t', index_col=0)
     l_data = loading_data.transpose()
-    new_cell_list = new_gmatrix_df.index.values
+    cell_list = gmatrix_df.index.tolist()
     cell_data = []
-    for cell in new_cell_list:
-
-        match_all = False
-        day_text = cell.split(split_on)[0]
-        tracking_id = cell
-        if day_text == 'D0':
-            day = 0
-            condition = 'ctrl'
-        elif day_text =='D4':
-            day = 4
-            condition = 'pnx'
-        elif day_text == 'D7':
-            day = 7
-            condition = 'pnx'
-        for s_cell in by_sample.columns.values:
-            if cell.split(split_on)[2] == s_cell.split('_')[2]:
-                total_mass = by_sample[s_cell][1]
-                for m_cell in by_cell_map.columns.values:
-                    if cell.split(split_on)[2] in m_cell:
-                        input_mass = by_cell_map[m_cell][0]
-                        per_mapped = by_cell_map[m_cell][4]
-                        for c_num in l_data.columns.values:
-                            if int(c_num) == int(cell.split(split_on)[2].strip('C')):
-                                match_all = True
-                                if day_text == 'D0':
-                                    loading = l_data[c_num][1]
-                                elif day_text == 'D4':
-                                    loading = l_data[c_num][0]
-                                elif day_text == 'D7':
-                                    loading = l_data[c_num][2]
-                                if loading == '1':
-                                    single_cell = 'yes'
-                                else:
-                                    single_cell = 'no'
-        if match_all:
-            c_data_tup = (tracking_id,total_mass,input_mass,per_mapped,condition,day,single_cell)
+    cell_label_dict ={'norm':('norm_ht280', 'ctrl'), 'scler':('scler_ht280', 'diseased'), 'IPF':('hu_IPF_HTII_280','diseased'), 'DK':('DK_ht280','diseased')}
+    new_cell_list = []
+    old_cell_list = []
+    for cell in cell_list:
+        match = False
+        if cell[0:4] == 'norm':
+            k = 'norm'
+            tracking_id = cell
+            match = True
+            num = cell.split('_')[2]
+            old_cell_list.append(cell)
+            new_cell_list.append(tracking_id)
+        elif cell[0:5] == 'scler':
+            k='scler'
+            tracking_id = cell
+            num = cell.split('_')[2]
+            match = True
+            old_cell_list.append(cell)
+            new_cell_list.append(tracking_id)
+        elif cell[0:2] == 'hu':
+            k = 'IPF'
+            tracking_id = cell
+            num = cell.split('_')[4]
+            match = True
+            old_cell_list.append(cell)
+            new_cell_list.append(tracking_id)
+        elif cell[0:2] == 'DK':
+            k = 'DK'
+            tracking_id = cell
+            num = cell.split('_')[2]
+            match = True
+            old_cell_list.append(cell)
+            new_cell_list.append(tracking_id)
+        if match:
+            condition = cell_label_dict[k][1]
+            loading_df = loading_data[cell_label_dict[k][0]]
+            print ret_loading_pos(num, plate_map_df), num
+            loading = loading_df.iloc[ret_loading_pos(num, plate_map_df)-1]
+            print num, tracking_id
+            if 'single' in loading:
+                single_cell = 'yes'
+            else:
+                single_cell = 'no'
+            print by_cell_map[cell]
+            total_mass = by_sample[cell+'_0'][1]
+            input_mass = by_cell_map[cell][0]
+            per_mapped = by_cell_map[cell][4]
+            c_data_tup = (tracking_id,total_mass,input_mass,per_mapped,condition,single_cell)
             print c_data_tup
             cell_data.append(c_data_tup)
-    cell_data_df = pd.DataFrame(cell_data, columns=['tracking_id','total_mass','input_mass','per_mapped','condition','day','single_cell'])
+    score_df.to_csv(os.path.join(path_to_file, 'gene_feature_data.txt'), sep = '\t', index=False)
+    new_cmatrix_df = cmatrix_df[old_cell_list]
+    new_cmatrix_df.columns = new_cell_list
+    new_cmatrix_df.to_csv(os.path.join(path_to_file, 'goterms_monocle_count_matrix.txt'), sep = '\t', index_col=0)
+    cell_data_df = pd.DataFrame(cell_data, columns=['tracking_id','total_mass','input_mass','per_mapped','condition','single_cell'])
     cell_data_df.to_csv(os.path.join(path_to_file, 'cell_feature_data.txt'), sep = '\t', index=False)
 
 make_new_matrix(df_by_gene1, gene_file_source)
