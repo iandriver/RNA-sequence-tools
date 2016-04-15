16c61698d0ec48d1ca221a2f7708427b65768cb5
idbedead <driver.ian@gmail.com>
2016-04-15 15:50:25 -0700

    added align filtering as option

--- FPKM_Parsing/filter_outliers.py
+++ FPKM_Parsing/filter_outliers.py
@@ -1,6 +1,6 @@
 import os
 import fnmatch
-import cPickle as pickle
+import pickle as pickle
 import numpy as np
 import pandas as pd
 from collections import OrderedDict
@@ -9,19 +9,21 @@ import matplotlib.pyplot as plt
 #This section will take fpkm matrix input and make pandas dataframe
 
 #path to fpkm file (usually cuffnorm output)
-path_to_file = '/Volumes/Seq_data/cuffnorm_hu_ht280_norm_a6'
+path_to_file = '/Volumes/Drobo/pdgfra_rsem_pnxd4'
 #default file name will use genes.fpkm_table from cuffnorm
-file_name = 'genes.fpkm_table'
+file_name = 'pdgfra_rsem_pnxd4_isoform_fpkm_matrix.txt'
 #provide base name for output files
-base_name ='hu_ht280_norm_a6'
+base_name ='fpkm_iso_pdgfra_all'
 #create pandas dataframe from fpkm files
 data = pd.DataFrame.from_csv(os.path.join(path_to_file,file_name), sep='\t')
 
 #This section is for making the alignment files
 
-
+#If tophat alignment files are not available or you don't want this option then use_align = False
+#If True will look for align
+use_align=False
 #the name of the file or files that contain the results from alignment, in a list
-result_file_names = ['results_norm_alpha6', 'results_DK_ht280', 'results_norm_ht280', 'results_scler_ht280', 'results_IPF_ht280']
+result_file_names = ['results_zw_141203_1','results_zw_141203_2', 'results_zw1_150612', 'results_zw_151125']
 #create full path to output file
 path_to_align=os.path.join(path_to_file, 'results_'+base_name+'_align.p')
 #change to True to force creation of new alignment even if file of same name already exists
@@ -30,7 +32,7 @@ run_even_if_align_exists = True
 #function for making alignment summary given one or more result files
 def make_align(result_file_names, base_name, path_to_file=path_to_file):
     #path to where the tophat result file or files are located, default is one level up from cuffnorm file
-    path = os.path.dirname(path_to_file)
+    path = '/Volumes/Seq_data'
     cell_list =[]
     align_dict =OrderedDict()
     align_dict['input_L_num'] = []
@@ -44,7 +46,7 @@ def make_align(result_file_names, base_name, path_to_file=path_to_file):
             for filename in fnmatch.filter(filenames, 'align_summary.txt'):
                 cell_name = (root.split('/')[-1])
                 cell_list.append(cell_name)
-                f = open(os.path.join(root,'align_summary.txt'), 'rw')
+                f = open(os.path.join(root,'align_summary.txt'), 'r')
                 for l in f:
                     if 'Left' in l:
                         side_s = 0
@@ -72,8 +74,8 @@ def make_align(result_file_names, base_name, path_to_file=path_to_file):
     align_df = pd.DataFrame(align_dict, index = cell_list)
     align_df.to_csv(os.path.join(path_to_file,'results_'+base_name+'_align.txt'), sep = '\t')
 
-    plt.hist(align_df['mapped_L_num'])
-    plt.show()
+    hist_plt = plt.hist(align_df['mapped_L_num'])
+    plt.close()
 
     with open(os.path.join(path_to_file,'results_'+base_name+'_align.p'), 'wb') as fp:
       pickle.dump(align_df, fp)
@@ -86,7 +88,7 @@ def delete_cells(by_cell, cell_list, del_list):
             to_delete1.append(pos)
     to_delete = sorted(to_delete1, reverse=True)
     for pos in to_delete:
-        print 'Deleted specific cell '+cell_list[pos]
+        print('Deleted specific cell '+cell_list[pos])
         del cell_list[pos]
     n_by_cell = np.delete(by_cell, to_delete, axis=0)
     return cell_list, n_by_cell
@@ -107,7 +109,6 @@ def filter_by_mapping(path_to_align, cutoff_per_map = 50, cutoff_metric='per_map
         for c in c_to_del:
             c2 = c.replace('-','_')
             nlist = c2.split('_')
-            print nlist, 'nlist'
             if nlist[0] == 'pdgfra':
                 if nlist[2] == 'ctrl':
                     new_c_to_del.append('Low_ctrl_'+nlist[3])
@@ -133,35 +134,26 @@ def filter_cells_sd(by_cell, cell_list, gene_cutoff=1000, sd=3.8):
     to_delete= []
     for cell_name, genes in zip(cell_list,by_cell):
         gen_exp = (genes >= 1).sum()
-        print gen_exp, cell_list.index(cell_name), cell_name
+        average_gene_exp.append(gen_exp)
         #if the cell doesn't express at least 'gene_cutoff' genes just delete it and exclude from average
         if gen_exp <=gene_cutoff:
             to_delete.append(cell_list.index(cell_name))
-        else:
-            average_gene_exp.append(gen_exp)
-    print len(to_delete)
     np_av = np.array(average_gene_exp)
     averg = np.average(np_av)
     gene_sd = np.std(np_av)
-    print averg, gene_sd
+    print('Average expression, stdev before: '+str(averg)+', '+str(gene_sd))
     #add cells that fall outside of stdev value sd
     for i1, exp_level in enumerate(np_av):
         if exp_level < (averg - (gene_sd*sd)) or exp_level > (averg + (gene_sd*sd)):
             if i1 not in to_delete:
                 to_delete.append(i1)
-    print len(to_delete)
     to_delete1 = sorted(to_delete, reverse = True)
-    print len(to_delete1), 'td2'
-    print to_delete1
-    print len(by_cell)
     for pos in to_delete1:
-        print len(cell_list)
-        print 'Deleted outlier '+cell_list[pos]
+        print('Deleted outlier '+cell_list[pos])
         del cell_list[pos]
     to_delete2 = sorted(to_delete1, reverse = False)
     by_cell = np.delete(by_cell, to_delete2, axis=0)
-    print len(by_cell), 'bc2'
-    print "Number of cells remaining: "+str(len(cell_list))
+    print("Number of cells remaining: "+str(len(cell_list)))
     naverage_gene_exp = []
     for ngenes in by_cell:
         ngen_exp = (ngenes >= 1).sum()
@@ -169,13 +161,12 @@ def filter_cells_sd(by_cell, cell_list, gene_cutoff=1000, sd=3.8):
     nnp_av = np.array(naverage_gene_exp)
     naverg = np.average(nnp_av)
     ngene_sd = np.std(nnp_av)
-    print "New", naverg, ngene_sd
+    print('Average expression, stdev new: '+str(naverg)+', '+str(ngene_sd))
     return cell_list, by_cell
 
 #delete genes that aren't expressed in at least 'number_expressed' cells at level of at least 'gene_express_cutoff' (default to 1)
-def threshold_genes(by_gene, gen_list, number_expressed=3, gene_express_cutoff=1.0):
+def threshold_genes(by_gene, gen_list, number_expressed=1, gene_express_cutoff=1.0):
     g_todelete = []
-    print gen_list
     for g1, gene in enumerate(by_gene):
         cells_exp = (gene >= gene_express_cutoff).sum()
         if str(gen_list[g1]) =='nan':
@@ -185,16 +176,11 @@ def threshold_genes(by_gene, gen_list, number_expressed=3, gene_express_cutoff=1
         if cells_exp < number_expressed:
             g_todelete.append(g1)
     g1_todelete = sorted(set(g_todelete), reverse = True)
-    print by_gene.shape
     for pos in g1_todelete:
-        print len(gen_list), pos, '2'
         if type(gen_list[pos]) != float:
-            print len(gen_list), pos
-            print 'Gene '+gen_list[pos]+' not expressed in '+str(number_expressed)+' cells.'
-
+            print('Gene '+gen_list[pos]+' not expressed in '+str(number_expressed)+' cells.')
         del gen_list[pos]
     n_by_gene = np.delete(by_gene, g1_todelete, axis=0)
-    print n_by_gene.shape
     return gen_list, n_by_gene
 
 #given a pandas dataframe of gene expression split out ERCC and return seperate dataframes of each
@@ -204,7 +190,7 @@ def sep_ERCC(pd_by_gene, gen_list):
     pop_list =[]
     to_del = []
     for i, gen in enumerate(w_gene_list):
-        if 'ERCC-00' == gen[0:7] or 'RNASPIKE1-EC02' in gen:
+        if 'ERCC-00' == gen[0:7] or 'RNASPIKE' in gen or 'XIST' in gen or 'TSIX' in gen:
             pop_list.append(i)
     to_del = sorted(to_del, reverse=True)
     for d in to_del:
@@ -217,34 +203,19 @@ def sep_ERCC(pd_by_gene, gen_list):
     return pd_by_gene_no_ERCC.transpose(), pd_ERCC.transpose(), w_gene_list
 
 #customized filtering when names are messed up
-def name_filtering(outlier_by_cell, outlier_cell_list):
+def name_filtering(outlier_by_cell, outlier_cell_list, filter_list, split_on = '_', pos=0):
+    delete_list_pos = []
+    delete_list_name = []
     for i, l in enumerate(outlier_by_cell):
-        split_cell_list = outlier_cell_list[i].split('_')
+        split_cell_list = outlier_cell_list[i].split(split_on)
         cell_name = outlier_cell_list[i]
-        if 'neg' in cell_name or '+' in cell_name or '-' in cell_name:
-            print cell_name, '1'
-            if 'Ra' not in split_cell_list and 'pdgfra' not in split_cell_list:
-                print cell_name, '2'
-                bulk_ctrl_dict['_'.join(split_cell_list[1:])] = [float(lix) for lix in l]
-        elif split_cell_list[0] == 'Lane1' and 'C' in split_cell_list[1] or split_cell_list[0] == 'Lane2' and 'C' in split_cell_list[1]:
-            if 'Ra' not in split_cell_list and 'pdgfra' not in split_cell_list:
-                if len(split_cell_list[1]) == 3:
-                    cell_name = 'CTRL_'+split_cell_list[1]
-                elif len(split_cell_list[1]) == 2:
-                    cell_name = 'CTRL_'+split_cell_list[1][0]+'0'+split_cell_list[1][1]
-                else:
-                    print split_cell_list[1], 'wtf'
-                outlier_fpkm_dict[cell_name] = [float(lx) for lx in l]
-        elif split_cell_list[0] == 'Lane3' and 'C' in split_cell_list[1] or split_cell_list[0] == 'Lane4'and 'C' in split_cell_list[1]:
-            if 'Ra' not in split_cell_list and 'pdgfra' not in split_cell_list:
-                if len(split_cell_list[1]) == 3:
-                    cell_name = 'PNX_'+split_cell_list[1]
-                elif len(split_cell_list[1]) == 2:
-                    cell_name = 'PNX_'+split_cell_list[1][0]+'0'+split_cell_list[1][1]
-                else:
-                    print split_cell_list[1], 'wtf'
-                outlier_fpkm_dict[cell_name] = [float(lx) for lx in l]
-    return outlier_fpkm_dict
+        for args in filter_list:
+            if args == split_cell_list[pos]:
+                delete_list_pos.append(i)
+                delete_list_name.append(cell_name)
+    new_cell_list = [c for c in outlier_cell_list if c not in delete_list_name]
+    new_outlier_by_cell = np.delete(outlier_by_cell, delete_list_pos, axis=0)
+    return new_outlier_by_cell, new_cell_list
 
 
 #if alignment file doesn't already exist run make alignment file
@@ -256,19 +227,20 @@ elif run_even_if_align_exists:
 else:
     pass
 #create cells to delete list. name_filter is an option for renaming if filenames are different between tophat and cuffnorm
-del_list=filter_by_mapping(path_to_align, name_filter=False)
-print del_list, 'del'
+if use_align:
+    del_list=filter_by_mapping(path_to_align, name_filter=False)
+else:
+    del_list=[]
+
 #create list of genes
 gen_list = data.index.tolist()
 
-print gen_list
 #make cell list, cuffnorm adds '_0' to end of file, so it will be removed, count files are left as is
 if file_name == 'genes.fpkm_table':
     cell_list = [x[:-2] for x in list(data.columns.values)]
 else:
     cell_list = [x for x in list(data.columns.values)]
 
-print cell_list , 'clist'
 #make dataframe a numpy array for easy manipulation
 npdata = np.array(data.values, dtype='f')
 npdata = np.nan_to_num(npdata)
@@ -290,22 +262,19 @@ filter_on_lane = False
 #function for removing cells by name, will be processed as a seperate matrix file
 bulk = False
 if filter_on_lane:
-    outlier_fpkm_dict = name_filtering(outlier_by_cell, outlier_cell_list)
-else:
-    print len(outlier_by_cell)
-    print len(outlier_cell_list)
-    for i, l in enumerate(outlier_by_cell):
-        print i
-        if bulk:
-            cell_name = outlier_cell_list[i]
-            #create for exclusion from final matrix and inclusion into bulk matrix
-            if 'bulk' in cell_name or '+' in cell_name or 'neg' in cell_name or '-' in cell_name:
-                bulk_ctrl_dict[cell_name] = [float(lx) for lx in l]
-            else:
-                outlier_fpkm_dict[cell_name] = [float(lx) for lx in l]
+    outlier_by_cell, outlier_cell_list = name_filtering(outlier_by_cell, outlier_cell_list, ['L', 'B'])
+
+for i, l in enumerate(outlier_by_cell):
+    if bulk:
+        cell_name = outlier_cell_list[i]
+        #create for exclusion from final matrix and inclusion into bulk matrix
+        if 'bulk' in cell_name or '+' in cell_name or 'neg' in cell_name or '-' in cell_name:
+            bulk_ctrl_dict[cell_name] = [float(lx) for lx in l]
         else:
-            cell_name = outlier_cell_list[i]
             outlier_fpkm_dict[cell_name] = [float(lx) for lx in l]
+    else:
+        cell_name = outlier_cell_list[i]
+        outlier_fpkm_dict[cell_name] = [float(lx) for lx in l]
 if bulk:
     df_bulk = pd.DataFrame(bulk_ctrl_dict, index = new_gene_list1)
 else:
@@ -319,10 +288,10 @@ if bulk:
 else:
     pass
 outlier_cell_list = [x for x in list(fpkm_df_outlier.columns.values)]
-df_ERCC.to_csv(os.path.join(path_to_file, base_name+'_ERCC.txt'), sep = '\t')
+df_ERCC.to_csv(os.path.join(path_to_file, base_name+'_ERCC2.txt'), sep = '\t')
 if bulk:
     df_bulk_ERCC.to_csv(os.path.join(path_to_file, base_name+'_bulkonly_ERCC.txt'), sep = '\t')
     bulk_ctrl_df.to_csv(os.path.join(path_to_file, base_name+'_bulk_ctrls.txt'), sep = '\t')
 else:
     pass
-fpkm_df_outlier.to_csv(os.path.join(path_to_file, base_name+'_outlier_filtered.txt'), sep = '\t')
+fpkm_df_outlier.to_csv(os.path.join(path_to_file, base_name+'_outlier_filtered2.txt'), sep = '\t')
