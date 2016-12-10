fd19ce15b67c63dddd16e7068d35e72a267c54a0
iandriver <driver.ian@gmail.com>
2016-12-10 09:24:29 -0800

    alignment parsing make report for hisat2

--- FPKM_Parsing/make_align_report_2.py
+++ FPKM_Parsing/make_align_report_2.py
@@ -12,25 +12,32 @@ result_file_names = ['results_sca_spc', 'results_spc2_n2']
 basename = 'sca_spc_combined'
 cell_list =[]
 align_dict =OrderedDict()
-align_dict['input_L_num'] = []
-align_dict['mapped_L_num'] = []
-align_dict['input_R_num'] = []
-align_dict['mapped_R_num'] = []
-align_dict['per_mapped'] = []
+align_dict['input_reads'] = []
+align_dict['num_condcord_0'] = []
+align_dict['per_condcord_0'] = []
+align_dict['num_condcord_exactly1'] = []
+align_dict['per_condcord_exactly1'] = []
+align_dict['num_condcord_g1'] = []
+align_dict['per_condcord_g1'] = []
+align_dict['per_overall'] = []
 for rf in result_file_names:
     path_to_file = os.path.join(path, rf)
     for root, dirnames, filenames in os.walk(path_to_file):
-        for filename in fnmatch.filter(filenames, 'align_summary.txt'):
-            cell_name = (root.split('/')[-1])
+        for filename in fnmatch.filter(filenames, '*.o*'):
+            cell_name = (filename.split('.')[0])
             cell_list.append(cell_name)
-            f = open(os.path.join(root,'align_summary.txt'), 'rw')
+            f = open(os.path.join(root,filename), 'r+')
             for l in f:
-                if 'Left' in l:
-                    side_s = 0
-                elif 'Right' in l:
-                    side_s = 1
-                if "Input" in l and side_s == 0:
-                    input_L_num = int(l.split(':')[-1])
+                if 'reads; of these:' in l:
+                    total_reads = l.split(' ')[0]
+                elif 'aligned concordantly 0 times' in l:
+                    concord0_num = l.split(' ')[0]
+                    concord0_per1 = l.split(')')[0]
+                    concord0_per2 = concord0_per1.split('(')[-1].strip('%')
+                if "aligned concordantly exactly 1 time" in l:
+                    concord1_num = l.split(' ')[0]
+                    concord1_per1 = l.split(')')[0]
+                    concord1_per2 = concord0_per1.split('(')[-1].strip('%')
                 if "Mapped" in l and side_s == 0:
                     mapped_L_1 = l.split(':')[-1]
                     mapped_L_num = int(mapped_L_1.split('(')[0].strip())
