7df2fff898f332b6097835790ba5782bd256d009
iandriver <driver.ian@gmail.com>
2017-01-02 15:11:53 -0800

    trims paired end files using Trimmomatic

--- Tophat_Cluster_submission/qsub_trim_PE.py
+++ Tophat_Cluster_submission/qsub_trim_PE.py
@@ -54,28 +54,65 @@ def qsub_submit(command_filename, hold_jobid = None, fname = None):
     return int(jobid)
 
 
-trim_file_name = 'krasnow_sra_18_5'
+trim_file_name = 'pengt_2nd'
 
 path = os.path.join('/netapp/home/idriver', trim_file_name)
 
-cuffdiff_files_1 = ''
-cuffdiff_files_rest = ''
+
 file_out= '${TMPDIR}'
-trim_fasta_path = '/netapp/home/idriver/Trimmomatic-0.36/adapters/NexteraPE-PE.fa'
+
 for root, dirnames, filenames in os.walk(path):
     if dirnames == []:
-        for files in filenames:
-            num = files.strip('.fastq.gz').split('_')[-1]
-            if num == '1':
-                pair_1 = os.path.join(root,files)
-            if num == '2':
-                pair_2 = os.path.join(root,files)
-        srr_num = os.path.basename(root)
-        file_name_end_list = ['_forward_paired.fq.gz','_forward_unpaired.fq.gz','_reverse_paired.fq.gz','_reverse_unpaired.fq.gz']
-        output_text = ' '
-        for extension in file_name_end_list:
-            output_text = output_text+file_out+'/'+srr_num+extension+' '
-        trim_cmd = 'java -jar /netapp/home/idriver/Trimmomatic-0.36/trimmomatic-0.36.jar PE -phred33 '+pair_1+' '+pair_2+output_text+'ILLUMINACLIP:${TMPDIR}/NexteraPE-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36'
+        fastq_files = [files for files in filenames if '.fastq.gz' in files]
+
+        name = os.path.basename(root)
+
+        #subprocess.call('mkdir -p '+path+'/%s' % name, shell=True)
+        if len(fastq_files) > 2:
+            r1_files = [r1 for r1 in fastq_files if r1.strip('.fastq.gz').split('_')[-2] == 'R1']
+            r1_files.sort()
+            r2_files = [r2 for r2 in fastq_files if r2.strip('.fastq.gz').split('_')[-2] == 'R2']
+            r2_files.sort()
+            r1_cat_cmd = 'cat '
+            r2_cat_cmd = 'cat '
+            for r1,r2 in zip(r1_files,r2_files):
+                r1_cat_cmd+= os.path.join(root,r1)+' '
+                r2_cat_cmd+= os.path.join(root,r2)+' '
+            r1_cat_cmd+= '> '+os.path.join(root,r1_files[0].strip('.fastq.gz')+'_new'+'.fastq.gz')
+            r2_cat_cmd+= '> '+os.path.join(root,r2_files[0].strip('.fastq.gz')+'_new'+'.fastq.gz')
+            print(r1_cat_cmd)
+            if os.path.isfile(os.path.join(root,r1_files[0].strip('.fastq.gz')+'_new'+'.fastq.gz')):
+                print(os.path.join(root,r1_files[0].strip('.fastq.gz')+'_new'+'.fastq.gz')+' exists.')
+                pass
+            else:
+                process = subprocess.Popen(r1_cat_cmd, stdout=subprocess.PIPE, shell=True)
+                out1, err1 = process.communicate()
+                print(out1)
+                print(r2_cat_cmd)
+                process = subprocess.Popen(r2_cat_cmd, stdout=subprocess.PIPE, shell=True)
+                out2, err2 = process.communicate()
+                print(out2)
+            pair_1 = os.path.join(root,r1_files[0].strip('.fastq.gz')+'_new'+'.fastq.gz')
+            pair_2 = os.path.join(root,r2_files[0].strip('.fastq.gz')+'_new'+'.fastq.gz')
+        if len(fastq_files) == 2:
+            for files in fastq_files:
+                num = files.strip('.fastq.gz').split('_')[-2]
+                if num == 'R1':
+                    pair_1 = os.path.join(root,files)
+                if num == 'R2':
+                    pair_2 = os.path.join(root,files)
+        if len(fastq_files) == 1:
+            trim_fasta_path = '/netapp/home/idriver/Trimmomatic-0.36/adapters/TruSeq3-SE.fa'
+            output_text = ' '+file_out+'/'+name+'_trimed_fastq.gz '
+            input_file = os.path.join(root,fastq_files[0])
+            trim_cmd = 'java -jar /netapp/home/idriver/Trimmomatic-0.36/trimmomatic-0.36.jar SE -phred33 '+input_file+output_text+'ILLUMINACLIP:${TMPDIR}/TruSeq3-SE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:75'
+        else:
+            trim_fasta_path = '/netapp/home/idriver/Trimmomatic-0.36/adapters/NexteraPE-PE.fa'
+            file_name_end_list = ['_forward_paired.fq.gz','_forward_unpaired.fq.gz','_reverse_paired.fq.gz','_reverse_unpaired.fq.gz']
+            output_text = ' '
+            for extension in file_name_end_list:
+                output_text = output_text+file_out+'/'+name+extension+' '
+            trim_cmd = 'java -jar /netapp/home/idriver/Trimmomatic-0.36/trimmomatic-0.36.jar PE -phred33 '+pair_1+' '+pair_2+output_text+'ILLUMINACLIP:${TMPDIR}/NexteraPE-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:70'
 
         command = """\
 #!/bin/sh
@@ -87,10 +124,10 @@ for root, dirnames, filenames in os.walk(path):
 #$ -cwd
 #$ -r y
 #$ -j y
-#$ -l netapp=10G,scratch=10G,mem_total=12G
-#$ -pe smp 6
+#$ -l netapp=5G,scratch=8G,mem_total=10G
+#$ -pe smp 4
 #$ -R yes
-#$ -l h_rt=1:59:00
+#$ -l h_rt=1:00:00
 set echo on
 date
 hostname
@@ -109,13 +146,13 @@ cp %(trim_fasta_path)s .
 %(trim_cmd)s
 # Copy the results back to the project directory:
 cd $TMPDIR
-cp -r %(srr_num)s* %(path)s/%(srr_num)s/
+cp -r %(name)s_* %(path)s/%(name)s/
 """ % vars()
-        if srr_num != '':
+        if name != '': #['BU3_C36_F9','BU3_C76_E12','ips17_C13_C3','ips17_C26_E2','ips17_C27_E3','ips17_C25_E1']:
             print(trim_cmd)
-            filename = 'trim_'+srr_num+'.sh'
+            filename = 'trim_'+name+'.sh'
             write_file(filename, command)
-            jobid = qsub_submit(filename, fname=srr_num)
+            jobid = qsub_submit(filename, fname=name)
             print "Submitted. jobid = %d" % jobid
             # Write jobid to a file.
             import subprocess
