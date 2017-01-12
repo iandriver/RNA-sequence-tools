62e6a525459b55c102738a543dc6caef4bfaeffb
iandriver <driver.ian@gmail.com>
2017-01-12 09:32:36 -0800

    update for both cell number and well number

--- file_rename_barcodes_v2.py
+++ file_rename_barcodes_v2.py
@@ -20,11 +20,12 @@ barcode_dict = {'TAAGGCGA':'N701','CGTACTAG':'N702','AGGCAGAA':'N703', 'TCCTGAGC
                 'TAGATCGC':'S501', 'CTCTCTAT':'S502', 'TATCCTCT':'S503', 'AGAGTAGA':'S504',
                 'GTAAGGAG':'S505', 'ACTGCATA':'S506', 'AAGGAGTA':'S507', 'CTAAGCCT':'S508',
                 'CGTCTAAT':'S510', 'TCTCTCCG':'S511', 'TCGACTAG':'S513', 'GCGTAAGA':'S517'}
-
-N_index1 = {'N701':'1', 'N702':'2', 'N703':'3', 'N704':'4', 'N705':'5', 'N706':'6', 'N707':'7', 'N710':'8',
-            'N711':'9', 'N712':'10', 'N714':'11', 'N715':'12'}
+#nextera V1 index map
+N_index1 = {'N701':'1', 'N702':'2', 'N703':'3', 'N704':'4', 'N705':'5', 'N706':'6', 'N707':'7', 'N708':'8',
+            'N709':'9', 'N710':'10', 'N711':'11', 'N712':'12'}
 S_index1 = {'S501':'A', 'S502':'B', 'S503':'C', 'S504':'D', 'S505':'E', 'S506':'F', 'S507':'G', 'S508':'H'}
 
+#nextera V2 index map
 N_index2 = {'N701':'1', 'N702':'2', 'N703':'3', 'N704':'4', 'N705':'5', 'N706':'6', 'N707':'7', 'N710':'8',
             'N711':'9', 'N712':'10', 'N714':'11', 'N715':'12'}
 S_index2 = {'S502':'A', 'S503':'B', 'S505':'C', 'S506':'D', 'S507':'E', 'S508':'F', 'S510':'G', 'S511':'H'}
@@ -55,7 +56,7 @@ for p in pats:
                     skip = True
                 if not skip:
                     name = cell_name+'_'+sindex+nindex
-                    
+
                     #uncomment when you actually want to move
                     #shutil.move(root,os.path.join(path_to_file,new_filename,name))
                     print(root, os.path.join(path_to_file,new_filename,name))
