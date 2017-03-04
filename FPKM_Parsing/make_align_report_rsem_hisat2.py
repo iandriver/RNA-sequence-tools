1eab6c10c2f8c488517b7f5f5d7e3114f8311055
iandriver <driver.ian@gmail.com>
2017-03-04 10:10:52 -0800

    now collects align stats with both rsem and hisat2 output

--- FPKM_Parsing/make_align_report_rsem_hisat2.py
+++ FPKM_Parsing/make_align_report_rsem_hisat2.py
@@ -3,12 +3,13 @@ import os
 import pandas as pd
 from scipy import stats
 import matplotlib as mpl
+mpl.use('Qt5Agg')
 import matplotlib.pyplot as plt
 from collections import OrderedDict
 
-path = '/Volumes/Drobo/Seq_data/rsem_pteng'
-result_file_names = ['rsem_pengt_2nd','rsem_pteng_1st']
-basename = 'rsem_m38_macs_cfms_pnxd7'
+path = '/Volumes/Drobo/Seq_data'
+result_file_names = ['results_hisat_aged_epi']
+basename = 'hisat2_aged_epi'
 cell_list =[]
 align_dict =OrderedDict()
 align_dict['input_reads'] = []
@@ -28,23 +29,24 @@ for rf in result_file_names:
 
             f = open(os.path.join(root,filename), 'r+')
             print(filename)
+            check1 = True
             for l in f:
                 if 'reads; of these:' in l:
                     total_reads = l.split(' ')[0]
                     check_file =True
-                if 'aligned concordantly 0 times' in l:
+                if 'aligned concordantly 0 times' in l and check1:
                     concord0_num = l.split(' ')[4]
-                    print(concord0_num)
                     concord0_per1 = l.split(')')[0]
                     concord0_per2 = concord0_per1.split('(')[-1].strip('%')
+                    check1 = False
                 if "aligned concordantly exactly 1 time" in l:
                     concord1_num = l.split(' ')[4]
                     concord1_per1 = l.split(')')[0]
-                    concord1_per2 = concord0_per1.split('(')[-1].strip('%')
+                    concord1_per2 = concord1_per1.split('(')[-1].strip('%')
                 if "aligned concordantly >1 times" in l:
                     concordg1_num = l.split(' ')[4]
                     concordg1_per1 = l.split(')')[0]
-                    concordg1_per2 = concord0_per1.split('(')[-1].strip('%')
+                    concordg1_per2 = concordg1_per1.split('(')[-1].strip('%')
                 if "overall alignment rate" in l:
                     overall_per = l.split('%')[0]
 
